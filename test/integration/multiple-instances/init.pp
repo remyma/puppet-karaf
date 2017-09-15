ce2941addd10133f78851ca5c6abe5b911943861
Matthieu REMY <marem@smile.fr>
2017-09-15 12:46:02 +0200

    Add test for multiple instances

--- test/integration/multiple-instances/init.pp
+++ test/integration/multiple-instances/init.pp
@@ -1,2 +1,24 @@
-class { 'java': }
-class { 'karaf': }
+class { '::karaf': }
+class { '::java': }
+
+karaf::instance { 'instance1':
+}
+
+karaf::instance { 'instance2':
+}
+
+## A hash of user resources:
+#$myKarafs = {
+#  'karaf1' => {
+#    karaf_http_port    => '8181',
+#    karaf_ssh_port     => '8101',
+#    karaf_file_name    => 'karaf1'
+#  },
+#  'karaf2'  => {
+#    karaf_http_port    => '8182',
+#    karaf_ssh_port     => '8102',
+#    karaf_file_name    => 'karaf2'
+#  },
+#}
+#
+#create_resources(karaf::instance, $myKarafs)
